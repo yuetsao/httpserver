f6d4ec255624f30630e4838e60f59fcc562c63d5
caoyue <yuetsao@foxmail.com>
2019-02-27 12:55:43 +0800

    Initial commit

---
+++ src/com/yuetsao/httpserver/core/BootStrap.java
@@ -0,0 +1,18 @@
+package com.yuetsao.httpserver.core;
+
+/**
+ * httpserver程序主入口
+ */
+public class BootStrap {
+    /**
+     * 主程序
+     * @param args
+     */
+    public static void main(String[] args) {
+        //程序入口
+        start();
+    }
+
+    private static void start() {
+    }
+}
